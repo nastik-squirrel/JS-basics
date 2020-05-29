f4de87d0c8ad697e15e5737bfefd021088cdc1f1
nastik-squirrel <46624815+nastik-squirrel@users.noreply.github.com>
2020-05-29 15:43:20 +0200

    Update homework3.js

--- Project/scripts/homework3.js
+++ Project/scripts/homework3.js
@@ -1,5 +1,20 @@
 function homework31() {
-    alert(`To be done!`);
+    let number = prompt('Input number:');
+    let result = +number % 2;
+    let text = '';
+    switch (result) {
+        case 0:
+            text = `${number} is EVEN`;
+            break;
+        case 1:
+            text = `${number} is ODD`;
+            break;
+        default:
+            text = `"${number}" is not a number`;
+            break;
+    }
+
+    alert(text);
 }
 
 function homework32() {

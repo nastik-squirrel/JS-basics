f10e1b66d5260de96a036fcf5cf946bb40f5b097
nastik-squirrel <46624815+nastik-squirrel@users.noreply.github.com>
2020-05-29 15:23:21 +0200

    Create homework3.js

---
+++ Project/scripts/homework3.js
@@ -0,0 +1,19 @@
+function homework31() {
+    alert(`To be done!`);
+}
+
+function homework32() {
+    alert(`To be done!`);
+}
+
+function homework33() {
+    alert(`To be done!`);
+}
+
+function homework34() {
+    alert(`To be done!`);
+}
+
+function homework35() {
+    alert(`To be done!`);
+}

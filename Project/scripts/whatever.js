726a46c05fba109911de36ab40011a7ed635441b
nastik-squirrel <46624815+nastik-squirrel@users.noreply.github.com>
2020-05-29 13:13:04 +0200

    Create whatever.js

---
+++ Project/scripts/whatever.js
@@ -0,0 +1,18 @@
+'use strict';
+
+function whatever1()
+{
+    console.log('some log');
+    prompt('input something here');
+    console.error('some error');
+    console.warn('some warning');
+}
+
+
+function whatever2()
+{
+    let number = 10
+    alert(`Number is ${number}`);
+
+    x = 5
+}

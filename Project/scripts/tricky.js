0567728dee582b22d974c6b49734d0a595b65b8e
nastik-squirrel <46624815+nastik-squirrel@users.noreply.github.com>
2020-06-02 08:33:09 +0200

    Update tricky.js

--- Project/scripts/tricky.js
+++ Project/scripts/tricky.js
@@ -1,12 +1,25 @@
-// 'use strict';
+function tricky1() {
+    let checkPrompt = (whatToCheck, correctValue, actionIfOK) => {
+        let value = prompt(`Provide ${whatToCheck}`)
+        if (value == correctValue) {
+            actionIfOK();
+        } else if (value == null) {
+            alert('Canceled');
+        } else {
+            alert(`Wrong ${whatToCheck}`);
+        }
+    }
 
-function tricky1()
-{
-    alert(`Nothing here yet`);
+    const correctLogin = 'Admin';
+    const correctPassword = 'TheMaster';
+
+    let checkPassword = () => checkPrompt('password', correctPassword, () => alert('Welcome'))
+    let checkLogin = () => checkPrompt('login', correctLogin, checkPassword)
+
+    checkLogin();
 }
 
 
-function tricky2()
-{
+function tricky2() {
     alert(`Nothing there as well`);
 }

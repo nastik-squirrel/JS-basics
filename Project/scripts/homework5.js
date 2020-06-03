17cf35e048876f0fb1ab00bc9c6a86720c4d1557
nastik-squirrel <46624815+nastik-squirrel@users.noreply.github.com>
2020-06-03 15:19:19 +0200

    Create homework5.js

---
+++ Project/scripts/homework5.js
@@ -0,0 +1,23 @@
+function homework51() {
+    console.log(`To be done`)
+}
+
+function homework52() {
+    console.log(`To be done`)
+}
+
+function homework53() {
+    console.log(`To be done`)
+}
+
+function homework54() {
+    console.log(`To be done`)
+}
+
+function homework55() {
+    console.log(`To be done`)
+}
+
+function homework56() {
+    console.log(`To be done`)
+}

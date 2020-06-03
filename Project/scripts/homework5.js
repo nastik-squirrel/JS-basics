3bf55e025514deb3f65de07af7055831328642a1
nastik-squirrel <46624815+nastik-squirrel@users.noreply.github.com>
2020-06-03 17:45:03 +0200

    Update homework5.js

--- Project/scripts/homework5.js
+++ Project/scripts/homework5.js
@@ -1,23 +1,89 @@
 function homework51() {
-    console.log(`To be done`)
+    
+    let fruits = ["Apples", "Pear", "Orange"];
+
+    // push a new value into the "copy"
+    let shoppingCart = fruits;
+    shoppingCart.push("Banana");
+
+    // what's in fruits?
+    alert(fruits.length); // ?
+    
+    console.warn(`(!)Reference copied, so original will be changed(!)`)
 }
 
 function homework52() {
-    console.log(`To be done`)
+    
+    let styles = ['Jazz', 'Blues'];
+    console.log(`New array created: ${styles}`);
+    
+    styles.push('Rock-n-Roll');
+    console.log(`New style added: ${styles}`);
+    
+    styles[1] = ('Classics');
+    console.log(`Middle style changed: ${styles}`);
+    
+    styles.splice(0, 0, 'Rap', 'Reggae');
+    console.log(`Prepend 2 new styles: ${styles}`);
+
 }
 
 function homework53() {
-    console.log(`To be done`)
+ 
+    function getAverageAge(array) {
+        let sum = array.reduce((sum, item) => sum + (+item), 0);
+        return sum/array.length;
+    }
+
+    let ageString = prompt('Provide ages list separated by comma');
+
+    let ageArray = ageString.split(',');
+    let average = getAverageAge(ageArray);
+    
+    alert(`Average is ${average}`);
+
 }
 
 function homework54() {
-    console.log(`To be done`)
+
+    function filterRange(array, a, b) {
+        newArray = []
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] >= a && array[i] <= b) {
+                newArray.push(array[i])                
+            }
+        }
+        return newArray
+    }
+
+    let initialArray = [4, 7, 44, 56, 198, 3, 55, 23, 14, 233];
+    let min = 12, max = 100;
+
+    filteredArray = filterRange(initialArray, min, max)
+
+    console.log(`Initial array: ${initialArray}`)
+    console.log(`Range: [${min} .. ${max}]`)
+    console.log(`Filtered array: ${filteredArray}`)
+    console.log(`Initial array unchanged: ${initialArray}`)
 }
 
 function homework55() {
-    console.log(`To be done`)
+
+    let extractCurrencyValue = str => +str.split('$')[1]
+    
+    let cost = prompt('Input cost with $:')
+    let value = extractCurrencyValue(cost)
+    
+    alert(`Currency value without sign is ${value}`)
 }
 
 function homework56() {
-    console.log(`To be done`)
+
+    let ucFirst = str => str[0].toUpperCase() + str.slice(1, str.length).toLowerCase()
+
+    let name = prompt('Input name:')
+    let corrected = ucFirst(name)   
+    
+    alert(`Correct name format is ${corrected}`)
+
 }

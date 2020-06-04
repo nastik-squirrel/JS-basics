036bb81f8bf6c091666f8d47c5c5a3e5578effa1
nastik-squirrel <46624815+nastik-squirrel@users.noreply.github.com>
2020-06-04 09:16:51 +0200

    Update tricky.js

--- Project/scripts/tricky.js
+++ Project/scripts/tricky.js
@@ -19,7 +19,43 @@ function tricky1() {
     checkLogin();
 }
 
-
 function tricky2() {
-    alert(`Nothing there as well`);
+    
+    // Forms an array with sums of n first integers
+    // [1, 1+2, 1+2+3, ... 1+2+..+n]
+
+    function getNSum(array, item) {
+        previous = array.length == 0 ? 0: array[array.length - 1]    
+        array.push(previous + item);
+        return array;
+    }
+
+    let n = 10
+    let simpleArray = []
+
+    for (let i = 1; i <= n; i++) {
+        simpleArray.push(i);
+    }
+    
+    console.log(simpleArray);
+    console.log(simpleArray.reduce(getNSum, []));
+}
+
+function tricky3() {
+    
+    // Forms a Fibonacci array:
+    // [1, 1, 2, 3, 5, 8 ...]
+
+    let n = 10
+    let fibonacciArray = [1]
+    let stored = 0, current = 0, next = 0
+
+    for (let i = 1; i <= n; i++) {
+        current = fibonacciArray[fibonacciArray.length - 1];
+        next = stored + current;
+        fibonacciArray.push(next);
+        stored = next;
+    }
+    
+    console.log(fibonacciArray);
 }

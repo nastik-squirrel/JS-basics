fe8f7b912523fe91c0db00b3f80376821f02dbc0
SEREDA Anastasiya (BCS/OCS) <anastasiya.sereda.ext@bics.com>
2020-06-12 19:33:45 +0200

    hw8 added (not completed) new files added

---
+++ Project/scripts/homework8.js
@@ -0,0 +1,19 @@
+function homework81() {
+    
+    console.log('To be done')
+}
+
+function homework82() {
+    
+    console.log('To be done')
+}
+
+function homework83() {
+    
+    console.log('To be done')
+}
+
+function homework84() {
+    
+    console.log('To be done')
+}

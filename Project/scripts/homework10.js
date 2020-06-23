e7a57ed4af5dfa3a4be8e8b6567c0fcd57bf5e2e
nastik-squirrel <nastikk@gmail.com>
2020-06-23 22:10:07 +0300

    Refactored according to comments

--- Project/scripts/homework10.js
+++ Project/scripts/homework10.js
@@ -1,11 +1,11 @@
-   
+let ball = document.getElementById("ball");
+
     function changeCoordinate(coordinate) {
         let a = (coordinate != "") ? parseInt(coordinate) : 0;
-        return a + 15 + "px";
+        return `${a + 15}px`;
     }
 
     function moveBallDiagonal() {
-        let ball = document.getElementById("ball");
         ball.style.left = changeCoordinate(ball.style.left);
         ball.style.bottom = changeCoordinate(ball.style.bottom);
     }
@@ -14,33 +14,50 @@
     ballButton.addEventListener("click", () => setTimeout(moveBallDiagonal, 500));
 
     function moveBallRight() {
-        let ball = document.getElementById("ball");
-        ball.style.left = ballInput.value + "px";
+        ball.style.left = `${ballInput.value}px`;
     }
 
     const ballInput = document.getElementById("moveBallX");
     ballInput.addEventListener("change", moveBallRight);
 
+    function addRemoveClass(eventTarget ,isIncorrect) {
+        isIncorrect ? eventTarget.classList.add("invalid") : eventTarget.classList.remove("invalid")
+    }
+
     function checkForm(event) {
-        let et = event.target;
-        if (et.type == "text") {
-            if (typeof(et.value) != "string" || et.value.length <3 || et.value.length > 20) {
-                console.error(`${et.name} should be a string from 3 to 20 characters`);
-                et.classList.add("invalid");
-            } else {
-                et.classList.remove("invalid");
-            }
-        } else if (et.type == "number") {
-            let v = parseInt(et.value, 10);
-            console.log(v)
-            if (isNaN(v) || v < 0 || v > 115) {
-                console.error(`${et.name} should be a number from 0 to 115`);
-                et.classList.add("invalid");
-            } else {
-                et.classList.remove("invalid");
-            }
+        if (event.target.type == "text") {
+            addRemoveClass(
+                event.target,
+                typeof(event.target.value) != "string" || event.target.value.length <3 || event.target.value.length > 20
+            )
+        } else if (event.target.type == "number") {
+            let v = parseInt(event.target.value, 10);
+            addRemoveClass(
+                event.target,
+                isNaN(v) || v < 0 || v > 115
+            )
         }
     }
+
+    // function checkForm(event) {
+    //     if (event.target.type == "text") {
+    //         if (typeof(event.target.value) != "string" || event.target.value.length <3 || event.target.value.length > 20) {
+    //             console.error(`${event.target.name} should be a string from 3 to 20 characters`);
+    //             event.target.classList.add("invalid");
+    //         } else {
+    //             event.target.classList.remove("invalid");
+    //         }
+    //     } else if (event.target.type == "number") {
+    //         let v = parseInt(event.target.value, 10);
+    //         console.log(v)
+    //         if (isNaN(v) || v < 0 || v > 115) {
+    //             console.error(`${event.target.name} should be a number from 0 to 115`);
+    //             event.target.classList.add("invalid");
+    //         } else {
+    //             event.target.classList.remove("invalid");
+    //         }
+    //     }
+    // }
 
     const form = document.getElementById("userForm");
     form.addEventListener("change", checkForm);
